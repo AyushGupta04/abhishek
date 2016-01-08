61d3d8e84120e13a5a48d2da1972dcf150efe1dd
abhishek <avishk92@gmail.com>
2016-01-08 12:17:52 +0530

    techi add

---
+++ techitechiboomboom/src/net/codejava/spring/model/User.java
@@ -0,0 +1,41 @@
+package net.codejava.spring.model;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+}
